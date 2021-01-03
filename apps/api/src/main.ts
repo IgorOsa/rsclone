d43640b9677f40d5ee143c8f12b1527cad02cbc5
Igor Osadchyi <igorosa.com@gmail.com>
2021-01-03 10:57:36 +0200

    feat: add static serving config for deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,19 @@
 import * as express from 'express';
+import * as path from 'path';
 import { Message } from '@memorio/api-interfaces';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../memorio')));
 
 const greeting: Message = { message: 'Welcome to api!' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../memorio', 'index.html'));
 });
 
 const port = process.env.PORT || 3333;

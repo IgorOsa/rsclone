7d428fda506ea073b754c2524dd9a904074a7984
AlexKelevrra <kelevrra1.0@ya.ru>
2021-01-16 18:30:33 +0300

    feat: multilang by i18n

--- apps/memorio/src/app/components/Header/Components/Language.tsx
+++ apps/memorio/src/app/components/Header/Components/Language.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DropdownButton, Dropdown, ButtonGroup } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
-export const Language = () => (
-  <DropdownButton style={{ }} as={ButtonGroup} title="Select Language" id="bg-nested-dropdown">
-    <Dropdown.Item eventKey="1">English</Dropdown.Item>
-    <Dropdown.Item eventKey="2">Russian</Dropdown.Item>
-  </DropdownButton>
-);
+export const Language = () => {
+  // const { t, i18n } = useTranslation();
+  // function handleClick(lang: string) {
+  //   i18n.changeLanguage(lang);
+  // } 
+
+  return (
+    <>
+      <DropdownButton style={{ }} as={ButtonGroup} title="Select Language" id="bg-nested-dropdown">
+        <Dropdown.Item eventKey="1" onClick={ () => handleClick('en') }>English</Dropdown.Item>
+        <Dropdown.Item eventKey="2" onClick={ () => handleClick('ru') }>Russian</Dropdown.Item>
+      </DropdownButton>
+      {/* <p>{t('title')}</p>s */}
+    </>
+  );
+};
 
 export default Language;

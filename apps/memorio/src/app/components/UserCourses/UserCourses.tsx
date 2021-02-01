33771e969743a6c5c368791cb822675bccaebdcb
Igor Osadchyi <igorosa.com@gmail.com>
2021-02-01 12:50:47 +0200

    refactor: redundant code crearing

--- apps/memorio/src/app/components/UserCourses/UserCourses.tsx
+++ apps/memorio/src/app/components/UserCourses/UserCourses.tsx
@@ -11,16 +11,16 @@ export default function UserCourses(): JSX.Element {
   return (
     <div className="user-courses">
       <h2 className="user-info__title">My courses:</h2>
-        <div className="card-group">
-          <a href="#" className="card" style={{ maxWidth: "300px" }}>
-            <img src="..." className="card-img-top" alt="course-name" style={{ height: "150px", background: "#f78410" }} />
-            <div className="card-body">
-              <h5 className="card-title">User_Course</h5>
-              <p className="card-text">Description of the course.</p>
-              <p className="card-text"><small className="text-muted">User_progress</small></p>
-            </div>
-          </a>
-        </div>
+      <div className="card-group">
+        <a href="#" className="card" style={{ maxWidth: "300px" }}>
+          <img src="..." className="card-img-top" alt="course-name" style={{ height: "150px", background: "#f78410" }} />
+          <div className="card-body">
+            <h5 className="card-title">User_Course</h5>
+            <p className="card-text">Description of the course.</p>
+            <p className="card-text"><small className="text-muted">User_progress</small></p>
+          </div>
+        </a>
+      </div>
       <a href="#" className="btn btn-primary user-courses__all">All courses</a>
     </div>
   );

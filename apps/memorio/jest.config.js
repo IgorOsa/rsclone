16cba9e3174e717b10a465b60534725f459d5149
Igor Osadchyi <igorosa.com@gmail.com>
2021-02-01 09:16:01 +0200

    fix: test suite config

--- apps/memorio/jest.config.js
+++ apps/memorio/jest.config.js
@@ -5,9 +5,13 @@ module.exports = {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': [
       'babel-jest',
-      { cwd: __dirname, configFile: './babel-jest.config.json' },
-    ],
+      { cwd: __dirname, configFile: './babel-jest.config.json' }
+    ]
   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/react-bootstrap/',
+    '<rootDir>/node_modules/react-i18next/'
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/memorio',
+  coverageDirectory: '../../coverage/apps/memorio'
 };

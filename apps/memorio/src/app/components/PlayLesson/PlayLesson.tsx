c5d55e9f21f923486201da413abf8f13b9502852
AlexKelevrra <kelevrra1.0@ya.ru>
2021-02-03 16:54:52 +0300

    feat: tests added

--- apps/memorio/src/app/components/PlayLesson/PlayLesson.tsx
+++ apps/memorio/src/app/components/PlayLesson/PlayLesson.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AudioPlay from '../AudioPlay/AudioPlay'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './play-lesson.scss'
@@ -11,41 +12,60 @@ export default function PlayLesson({ lessonNum }): JSX.Element {
   let [right, setRight] = useState('')
   let [wrong, setWrong] = useState('')
   let [answer, setAnswer] = useState('')
+  let [rightModal, setRightModal] = useState('')
   const taskAnswer = levels[lessonNum].task[task].answer
+  let [audio, setAudio] = useState('')
 
   return (
     <main className="lesson" style={{ minHeight: "calc(100vh - 160px)" }}>
       <div className="container">
-          <h2 className="lesson__title">{ levels[lessonNum].task[task].title }</h2>
-          <div className="lesson__body">{ levels[lessonNum].task[task].test.map((it: string, i: number) => (
+        { !rightModal ? (
+          <>
+            <h2 className={`lesson__title ${wrong}`}>{ levels[lessonNum].task[task].title }</h2>
+            <div className="lesson__body">{ levels[lessonNum].task[task].test.map((it: string, i: number) => (
+              <button 
+                className={`btn btn-outline-secondary`}
+                type="button" 
+                key={i}
+                onClick={() => {
+                  if(it === levels[lessonNum].task[task].answer) {
+                    setRight('right')
+                    setAnswer('')
+                    setRightModal('Верно!')
+                    setAudio(levels[lessonNum].task[task].audio)
+                    setTimeout(() => {
+                      setRightModal('')
+                      setTask(task += 1)
+                    }, 500);
+                  } else {
+                    setAudio('error.mp3')
+                    setWrong('wrong')
+                      setTimeout(() => {
+                        setAudio('')
+                        setWrong('s')
+                      }, 1000)
+                  }
+                }}
+              >
+                {it}
+              </button>
+            )) }</div>
             <button 
-              className={`btn btn-outline-secondary ${right} ${wrong}`}
-              type="button" 
-              key={i}
+              className="btn btn-outline-secondary show-answer"
+              type="button"
               onClick={() => {
-                if(it === levels[lessonNum].task[task].answer) {
-                  setRight('right')
-                  setAnswer('')
-                  setTimeout(() => {
-                    setTask(task += 1)
-                  }, 500);
-                }
+                answer === taskAnswer ? setAnswer('') : setAnswer(taskAnswer)
               }}
             >
-              {it}
+              Посмотреть ответ
             </button>
-          )) }</div>
-          <button 
-            className="btn btn-outline-secondary"
-            type="button"
-            onClick={() => {
-              answer === taskAnswer ? setAnswer('') : setAnswer(taskAnswer)
-            }}
-          >
-            Посмотреть ответ
-          </button>
-          <h2 style={{ marginTop: '10px' }}>{ answer }</h2>
+            <h2 style={{ marginTop: '10px' }}>{ answer }</h2>
+          </>
+        ) : (
+          <h2 className="lesson__title right">{ rightModal }</h2>
+        ) }
       </div>
+      <AudioPlay audio={ audio } />
     </main>
   );
 }
